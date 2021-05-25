85252e8e44a6ccb3e0aec93e086eef074814a6c9
Maarten Braakhekke <maarten.braakhekke@wur.nl>
2021-05-25 14:00:56 +0200

    Results from running trial sources during CMake config are now stored in the CMake cache

--- cmake/CheckCompilerCapabilities.cmake
+++ cmake/CheckCompilerCapabilities.cmake
@@ -3,19 +3,23 @@ include (${PROJECT_SOURCE_DIR}/cmake/CheckFortranSource.cmake)
 CHECK_FORTRAN_SOURCE_RUN (
   ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/LOGICAL_DEFAULT_KIND.F90
   _LOGICAL_DEFAULT_KIND
+  "Default logical kind"
   )
 
 CHECK_FORTRAN_SOURCE_RUN (
   ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/INT_DEFAULT_KIND.F90
   _INT_DEFAULT_KIND
+  "Default integer kind"
   )
 foreach (kind 8 16 32 64)
+
   set(CMAKE_REQUIRED_FLAGS = -fpp)
   set(CMAKE_REQUIRED_DEFINITIONS -D_KIND=INT${kind})
 
   CHECK_FORTRAN_SOURCE_RUN (
     ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/INT_KIND.F90
     _ISO_INT${kind}
+    "Integer(${kind}) kind"
     )
 
 endforeach()
@@ -23,41 +27,64 @@ endforeach()
 CHECK_FORTRAN_SOURCE_RUN (
   ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_DEFAULT_KIND.F90
   _REAL_DEFAULT_KIND
+  "Default real kind"
   )
 
 CHECK_FORTRAN_SOURCE_RUN (
   ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/DOUBLE_DEFAULT_KIND.F90
   _DOUBLE_DEFAULT_KIND
+  "Default double kind"
   )
 
 foreach (kind 32 64 80 128 256)
-  set(CMAKE_REQUIRED_FLAGS = -fpp)
-  set(CMAKE_REQUIRED_DEFINITIONS -D_KIND=REAL${kind})
 
-  try_compile (
-    code_compiles
-    ${CMAKE_BINARY_DIR}
-    ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
-    CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}")
+  if (DEFINED CACHE{PFUNIT_ISO_REAL${kind}})
   
-  if (code_compiles)
-    CHECK_FORTRAN_SOURCE_RUN (
+	set(_ISO_REAL${kind} ${PFUNIT_ISO_REAL${kind}})
+    add_definitions(-D_ISO_REAL${kind}=${_ISO_REAL${kind}})
+    
+  else ()
+  
+    set(CMAKE_REQUIRED_FLAGS = -fpp)
+    set(CMAKE_REQUIRED_DEFINITIONS -D_KIND=REAL${kind})
+
+    try_compile (
+      code_compiles
+      ${CMAKE_BINARY_DIR}
       ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
-      _ISO_REAL${kind}
-      )
+      CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}")
+    
+    if (code_compiles)
+      CHECK_FORTRAN_SOURCE_RUN (
+        ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
+        _ISO_REAL${kind}
+        "Real(${kind}) kind"
+        )
+    endif ()
+    
   endif ()
   
-  try_compile (
-    code_compiles
-    ${CMAKE_BINARY_DIR}
-    ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
-    CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}")
+  if (DEFINED CACHE{PFUNIT_REAL${kind}_IEEE_SUPPORT})
   
-  if (code_compiles)
-    CHECK_FORTRAN_SOURCE_RUN(
-      ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND_IEEE_SUPPORT.F90
-      _REAL${kind}_IEEE_SUPPORT
-      )
+    add_definitions(-D_REAL${kind}_IEEE_SUPPORT=${PFUNIT_REAL${kind}_IEEE_SUPPORT})
+    
+  else ()
+    
+    try_compile (
+      code_compiles
+      ${CMAKE_BINARY_DIR}
+      ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND.F90
+      CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}")
+    
+    if (code_compiles)
+      CHECK_FORTRAN_SOURCE_RUN(
+        ${PROJECT_SOURCE_DIR}/cmake/Trial_sources/REAL_KIND_IEEE_SUPPORT.F90
+        _REAL${kind}_IEEE_SUPPORT
+        "Support for IEEE real(${kind}) kind"
+        )
+        
+    endif ()
+   
   endif ()
 
 endforeach()
--- cmake/CheckFortranSource.cmake
+++ cmake/CheckFortranSource.cmake
@@ -1,28 +1,40 @@
-macro (CHECK_FORTRAN_SOURCE_RUN file var)
+macro (CHECK_FORTRAN_SOURCE_RUN file var docstr)
 
-  try_run (
-    run compile
-    ${CMAKE_BINARY_DIR}
-    ${file}
-    CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}"
-    RUN_OUTPUT_VARIABLE ${var}
-    )
-
-  # Successful runs return "0", which is opposite of CMake sense of "if":
-  if (NOT run)
-    string(STRIP "${${var}}" ${var})
-    if (NOT CMAKE_REQUIRED_QUIET)
-      message(STATUS "Performing Test ${var}: SUCCESS (value=${${var}})")
-    endif ()
-    
-    add_definitions(-D${var}=${${var}})
+  if (DEFINED CACHE{PFUNIT${var}})
+  
+    set( ${var} ${PFUNIT${var}})
     
   else ()
-    
-    if (NOT CMAKE_REQUIRED_QUIET)
-      message(STATUS "Performing Test ${var}: FAILURE")
+   
+    try_run (
+      run compile
+      ${CMAKE_BINARY_DIR}
+      ${file}
+      CMAKE_FLAGS "-DCOMPILE_DEFINITIONS=${CMAKE_REQUIRED_DEFINITIONS}"
+      RUN_OUTPUT_VARIABLE ${var}
+      )
+
+    # Successful runs return "0", which is opposite of CMake sense of "if":
+    if (NOT run)
+      string(STRIP "${${var}}" ${var})
+      if (NOT CMAKE_REQUIRED_QUIET)
+        message(STATUS "Performing Test ${var}: SUCCESS (value=${${var}})")
+      endif ()
+      
+      set ( PFUNIT${var} ${${var}} CACHE STRING ${docstr} )
+      
+    else ()
+      
+      if (NOT CMAKE_REQUIRED_QUIET)
+        message(STATUS "Performing Test ${var}: FAILURE")
+      endif ()
+      
     endif ()
     
+  endif ()
+   
+  if (NOT ${${var}} EQUAL "")
+    add_definitions(-D${var}=${${var}})
   endif ()
 
 endmacro (CHECK_FORTRAN_SOURCE_RUN)
